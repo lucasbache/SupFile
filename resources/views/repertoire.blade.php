183c821744268b839a7197db40f314f2b6f3e633
Lucas <lucasdollar@hotmail.fr>
2018-05-22 18:39:54 +0200

    Preview fichier txt

--- resources/views/repertoire.blade.php
+++ resources/views/repertoire.blade.php
@@ -247,26 +247,6 @@
                         </div>
                     </div>
 
-                    <!-- The Modal doc -->
-                    <div class="modal fade" id="modal-docx" tabindex="-1" role="dialog"
-                         aria-labelledby="modal-video-label">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="modal-docx">
-                                        <p id="myDocx">
-                                            {{File::get($File->cheminFichier)}}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
